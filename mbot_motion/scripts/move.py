734682bd51dff046a41626453bd45ea851751097
Sylwia Bakalarz <bakalarz.sylwia2@gmail.com>
2019-04-08 11:16:36 +0200

    Update move.py

--- mbot_motion/scripts/move.py
+++ mbot_motion/scripts/move.py
@@ -11,11 +11,14 @@ def move():
 
     # creates a publisher
     pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+    
+    linear_velocity = 0.2
+    angular_velocity = 0.2
 
     # creates a Twist message with linear and angular values.
     msg = Twist()
-    msg.linear.x = 0.2
-    msg.angular.z = 0.2
+    msg.linear.x = linear_velocity
+    msg.angular.z = angular_velocity
 
     # save current time and publish rate at 10 Hz.
     start = rospy.Time.now()
